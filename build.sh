334824e7dc9486ab0e593d68abb6403faa5d92a6
gabriel <gabriel@buildserver.corp.microsoft.com>
2014-05-21 16:05:27 -0700

    finished build script

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-cd ./setuptools
-./build.sh
+cd ./setuptools && ./build.sh && cd ..
+cd ./eventlet && ./build.sh && cd ..
+cd ./greenlet && ./build.sh && cd ..
+cd ./lxml && ./build.sh && cd ..
+cd ./m2crypto && ./build.sh && cd ..
+cd ./mysql-python && ./build.sh && cd ..
+cd ./numpy && ./build.sh && cd ..
+cd ./posix-ipc && ./build.sh && cd ..
+cd ./pycrypto && ./build.sh && cd ..
+cd ./pyopenssl && ./build.sh && cd ..
+
